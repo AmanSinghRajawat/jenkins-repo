18ba91aa43707377dafcc37f8b0386c95bb4067b
AmanSinghRajawat <iamamansingh17@gmail.com>
2024-03-04 15:31:03 +0530

    adding date into main method

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.util.Date;
 
 public class Main {
         public static void main(String args[]){
             System.out.println("Hello Jenkins...");
             System.out.println("second line printing...");
+            System.out.println(new Date());
         }
 }
